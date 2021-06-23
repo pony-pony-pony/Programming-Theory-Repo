aa8b0d51eee8cb44acd828a35881c95a4423172a
pony-pony-pony <84202832+pony-pony-pony@users.noreply.github.com>
2021-06-23 21:44:13 +0300

    title screen scene commit
     - added UIHandler script
     - added method that handles Start button click
    To sum up, first scene is done.

---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIHandler : MonoBehaviour
+{
+    public void StartButtonClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    
+
+
+}
